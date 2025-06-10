7d5d045ef201202ec5317bc52a37394a2b7e6086
Yaseen Sheikh <official.yaseen.sheikh@gmail.com>
2025-06-10 17:33:34 +0530

    #1 SpringBoot setup.

---
+++ src/main/java/book/my/film/code/bookmyfilm/BookMyFilmApplication.java
@@ -0,0 +1,13 @@
+package book.my.film.code.bookmyfilm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMyFilmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookMyFilmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/book/my/film/code/bookmyfilm/BookMyFilmApplicationTests.java
@@ -0,0 +1,13 @@
+package book.my.film.code.bookmyfilm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMyFilmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
